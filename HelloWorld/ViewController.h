665b7a3d2ce25b0df539fb278473f26ace0c34dd
Breno Medeiros <breno.medeiros@icloud.com>
2018-10-15 18:55:39 -0300

    Initial Commit

---
+++ HelloWorld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloWorld
+//
+//  Created by Breno Medeiros de Oliveira on 09/10/2018.
+//  Copyright © 2018 Breno Medeiros de Oliveira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HelloWorld/ViewController.m
@@ -0,0 +1,38 @@
+//
+//  ViewController.m
+//  HelloWorld
+//
+//  Created by Breno Medeiros de Oliveira on 09/10/2018.
+//  Copyright © 2018 Breno Medeiros de Oliveira. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+- (IBAction)hideView:(id)sender;
+- (IBAction)showView:(id)sender;
+@property (weak, nonatomic) IBOutlet UIView *workingView;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (IBAction)hideView:(id)sender {
+    [UIView beginAnimations:nil context:nil];
+    self.workingView.alpha = 0;
+    [UIView commitAnimations];
+}
+
+- (IBAction)showView:(id)sender {
+    [UIView beginAnimations:nil context:nil];
+    self.workingView.alpha = 1;
+    [UIView commitAnimations];
+}
+@end
